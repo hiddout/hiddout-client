9754316ed110af6d30b50aaebd6fc6f25911a5c5
TyrealGray <tyrealgray@ymail.com>
2019-02-25 18:16:05 +0000

    change words 'start' to 'dev'

--- devScript/dev.js
+++ devScript/dev.js
@@ -2,7 +2,7 @@ const util = require('./util');
 
 const platform = /^win/.test(process.platform) ? 'win' : 'mac';
 
-util.childProcessRunNPM(['run', `start-${platform}`], process.cwd()).then((code) => {
+util.childProcessRunNPM(['run', `dev-${platform}`], process.cwd()).then((code) => {
 	console.log(`Code: ${code}`);
 }).catch(e => {
 	console.log(`ERROR: ${e}`);
