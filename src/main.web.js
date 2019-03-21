e6be6181b83c7cec158b10ab0662634c79fe75a6
TyrealGray <tyrealgray@ymail.com>
2019-03-21 20:35:20 +0000

    change way of loading extension

--- src/main.web.js
+++ src/main.web.js
@@ -2,4 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const viewerWorker = new Worker('/public/js/hiddout-extension.js');
+
+viewerWorker.postMessage('init');
+
+viewerWorker.onmessage = (e) => {
+	if (typeof hiddoutViewer !== 'undefined') {
+		return;
+	}
+
+	const template = `<script type="text/javascript">${e.data}var hiddoutViewer = window.index.HiddoutViewer;</script>`;
+	const frag = document.createRange().createContextualFragment(template);
+	const head = document.getElementsByTagName('head').item(0);
+	head.appendChild(frag);
+};
+
+const interval = setInterval(() => {
+	if (typeof hiddoutViewer !== 'undefined') {
+		clearInterval(interval);
+
+		ReactDOM.render(<App />, document.getElementById('root'));
+	}
+}, 500);
