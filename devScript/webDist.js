df358eabd24994c5154b4f2bf86385436324d59e
TyrealGray <tyrealgray@ymail.com>
2019-03-23 18:21:59 +0000

    change webDist.js

--- devScript/webDist.js
+++ devScript/webDist.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const fsExtra = require('fs-extra');
 
-const webSrcName = 'hiddout.js';
+const webFolderSrc = 'public';
+const indexHTML = 'index.html';
 
-const distPath = path.join(process.cwd(), 'dist');
-const webSrc = path.join(process.cwd(), 'public', 'js', webSrcName);
+const distPath = path.join(process.cwd(), 'dist', 'web');
+const webSrc = path.join(process.cwd(), webFolderSrc),
+	indexSrc = path.join(process.cwd(), indexHTML);
 
 fsExtra.ensureDirSync(distPath);
-fsExtra.copySync(webSrc, path.join(distPath, webSrcName));
+fsExtra.copySync(webSrc, path.join(distPath, webFolderSrc));
+fsExtra.copySync(indexSrc, path.join(distPath, webFolderSrc, indexHTML));
