8c424a69e8818f45b1e65d0414f348b9171f8828
TyrealGray <tyrealgray@ymail.com>
2019-05-01 23:12:03 +0100

    do not let user up vote own post

--- src/pages/postPage/Post.js
+++ src/pages/postPage/Post.js
@@ -18,7 +18,12 @@ import { submitComment, submitReaction } from '../../actions/submitActions';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import SubmitForm from '../../component/submitForm/SubmitForm';
-import { COMMENT_SUBMITTED, GET_COMMENTS, GET_POST, REACTION_REACTED } from '../../actions/actionType';
+import {
+	COMMENT_SUBMITTED,
+	GET_COMMENTS,
+	GET_POST,
+	REACTION_REACTED,
+} from '../../actions/actionType';
 
 const ReactMarkdown = React.lazy(() => import('react-markdown'));
 
@@ -56,7 +61,6 @@ const REACT_DOWN = 'down';
 const REACT_LOL = 'lol';
 
 class Post extends React.Component<Props, State> {
-
 	state = {
 		submitting: false,
 		reacted: false,
@@ -67,13 +71,13 @@ class Post extends React.Component<Props, State> {
 		const id = this.props.match.params.id;
 		const postRes = await this.props.getPost(id);
 
-		if(postRes.type !== GET_POST) {
+		if (postRes.type !== GET_POST) {
 			return;
 		}
 
 		const commentRes = await this.props.getComments(id);
 
-		if(commentRes.type !== GET_COMMENTS){
+		if (commentRes.type !== GET_COMMENTS) {
 			return;
 		}
 
@@ -96,7 +100,6 @@ class Post extends React.Component<Props, State> {
 					<Placeholder.Line />
 				</Placeholder.Paragraph>
 			</Placeholder>
-
 		);
 
 		return (
@@ -115,42 +118,48 @@ class Post extends React.Component<Props, State> {
 	}
 
 	reactPost(reaction: string) {
-		const {auth, account, post} = this.props;
+		const { auth, account, post } = this.props;
 
-		if(!auth.isAuth || !post.reactions /*|| account.user === post.currentPost.userId*/ ){
+		if (
+			!auth.isAuth ||
+			!post.reactions || account.user === post.currentPost.userId
+		) {
 			return;
 		}
 
-		this.setState({
-			reacted: true,
-			reaction,
-		}, async ()=>{
-			const response = await this.props.submitReaction({
-				postId:this.props.match.params.id,
-				userId: account.user,
+		this.setState(
+			{
+				reacted: true,
 				reaction,
-			});
+			},
+			async () => {
+				const response = await this.props.submitReaction({
+					postId: this.props.match.params.id,
+					userId: account.user,
+					reaction,
+				});
 
-			if (response.type !== REACTION_REACTED) {
-				return;
-			}
+				if (response.type !== REACTION_REACTED) {
+					return;
+				}
 
-			this.props.getReactions(this.props.match.params.id);
-			this.setState({
-				reacted: false,
-			});
-		});
-
+				this.props.getReactions(this.props.match.params.id);
+				this.setState({
+					reacted: false,
+				});
+			},
+		);
 	}
 
 	getReactionButtons(): Node {
-
 		const { currentPost, reactions } = this.props.post;
 
-		if(!currentPost || !reactions){
-			return (<Placeholder style={{ height: 40, width: 200 }}>
-				<Placeholder.Image />
-			</Placeholder>);
+		if (!currentPost || !reactions) {
+			return (
+				<Placeholder style={{ height: 40, width: 200 }}>
+					<Placeholder.Image />
+				</Placeholder>
+			);
 		}
 
 		let reacted = null,
@@ -158,11 +167,11 @@ class Post extends React.Component<Props, State> {
 			disableDown = false,
 			disableLOL = false;
 
-		for(const reactInfo of reactions) {
-				if(reactInfo.userId === this.props.account.user){
-					reacted = reactInfo.reaction;
-					break;
-				}
+		for (const reactInfo of reactions) {
+			if (reactInfo.userId === this.props.account.user) {
+				reacted = reactInfo.reaction;
+				break;
+			}
 		}
 
 		switch (reacted) {
@@ -179,61 +188,77 @@ class Post extends React.Component<Props, State> {
 				break;
 		}
 
-		return (<React.Fragment>
-			<Popup
-				trigger={
-					<Button
-						color="green"
-						icon="arrow alternate circle up outline"
-						label={{
-							basic: true,
-							color: 'green',
-							pointing: 'left',
-							content: currentPost.up + ((this.state.reaction === REACT_UP )? 1 : 0),
-						}}
-						disabled={disabledUp || this.state.reacted}
-						onClick={()=>{this.reactPost(REACT_UP);}}
-					/>
-				}
-				content={REACT_UP}
-			/>
+		return (
+			<React.Fragment>
+				<Popup
+					trigger={
+						<Button
+							color="green"
+							icon="arrow alternate circle up outline"
+							label={{
+								basic: true,
+								color: 'green',
+								pointing: 'left',
+								content:
+									currentPost.up +
+									(this.state.reaction === REACT_UP ? 1 : 0),
+							}}
+							disabled={disabledUp || this.state.reacted}
+							onClick={() => {
+								this.reactPost(REACT_UP);
+							}}
+						/>
+					}
+					content={REACT_UP}
+				/>
 
-			<Popup
-				trigger={
-					<Button
-						color="red"
-						icon="arrow alternate circle down outline"
-						label={{
-							basic: true,
-							color: 'red',
-							pointing: 'left',
-							content: currentPost.down + ((this.state.reaction === REACT_DOWN )? 1 : 0),
-						}}
-						disabled={disableDown || this.state.reacted}
-						onClick={()=>{this.reactPost(REACT_DOWN);}}
-					/>
-				}
-				content={REACT_DOWN}
-			/>
+				<Popup
+					trigger={
+						<Button
+							color="red"
+							icon="arrow alternate circle down outline"
+							label={{
+								basic: true,
+								color: 'red',
+								pointing: 'left',
+								content:
+									currentPost.down +
+									(this.state.reaction === REACT_DOWN
+										? 1
+										: 0),
+							}}
+							disabled={disableDown || this.state.reacted}
+							onClick={() => {
+								this.reactPost(REACT_DOWN);
+							}}
+						/>
+					}
+					content={REACT_DOWN}
+				/>
 
-			<Popup
-				trigger={
-					<Button
-						color="black"
-						icon="child"
-						label={{
-							basic: true,
-							color: 'black',
-							pointing: 'left',
-							content: currentPost.lol + ((this.state.reaction === REACT_LOL)? 1 : 0),
-						}}
-						disabled={disableLOL || this.state.reacted}
-						onClick={()=>{this.reactPost(REACT_LOL);}}
-					/>
-				}
-				content={REACT_LOL}
-			/>
-		</React.Fragment>);
+				<Popup
+					trigger={
+						<Button
+							color="black"
+							icon="child"
+							label={{
+								basic: true,
+								color: 'black',
+								pointing: 'left',
+								content:
+									currentPost.lol +
+									(this.state.reaction === REACT_LOL ? 1 : 0),
+							}}
+							disabled={disableLOL || this.state.reacted}
+							onClick={() => {
+								this.reactPost(REACT_LOL);
+							}}
+						/>
+					}
+					content={REACT_LOL}
+				/>
+			</React.Fragment>
+		);
 	}
 
 	getContent(): Node {
@@ -267,7 +292,13 @@ class Post extends React.Component<Props, State> {
 					<Divider />
 
 					{!!comments.length && !!replyTo && (
-						<Message floating color='yellow' content={`re: @${comments[replyTo - 1].userId} - ${comments[replyTo - 1].content}`} />
+						<Message
+							floating
+							color="yellow"
+							content={`re: @${comments[replyTo - 1].userId} - ${
+								comments[replyTo - 1].content
+							}`}
+						/>
 					)}
 
 					{this.props.auth.isAuth && (
@@ -282,19 +313,23 @@ class Post extends React.Component<Props, State> {
 									postId: this.props.match.params.id,
 								};
 
-								this.setState({submitting: true},() => {
+								this.setState({ submitting: true }, () => {
 									this.props
 										.submitComment(commentData)
 										.then((response: Object) => {
 											if (
-												response.type === COMMENT_SUBMITTED
+												response.type ===
+												COMMENT_SUBMITTED
 											) {
-												this.props.getComments(this.props.match.params.id);
-												this.setState({submitting: false});
+												this.props.getComments(
+													this.props.match.params.id,
+												);
+												this.setState({
+													submitting: false,
+												});
 											}
 										});
 								});
-
 							}}
 						/>
 					)}
@@ -309,7 +344,10 @@ class Post extends React.Component<Props, State> {
 		return (
 			<React.Fragment>
 				<NavigationBar showBackBtn={true} />
-				<Container textAlign={'left'} style={{ marginTop: '7em', marginBottom:'3em' }}>
+				<Container
+					textAlign={'left'}
+					style={{ marginTop: '7em', marginBottom: '3em' }}
+				>
 					<Segment>{this.getContent()}</Segment>
 				</Container>
 			</React.Fragment>
@@ -328,7 +366,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
 	return {
 		submitComment: (commentData) => dispatch(submitComment(commentData)),
-		submitReaction: (reactionData) => dispatch(submitReaction(reactionData)),
+		submitReaction: (reactionData) =>
+			dispatch(submitReaction(reactionData)),
 		getPost: (id) => dispatch(getPost(id)),
 		getReactions: (id) => dispatch(getReactions(id)),
 		getComments: (id) => dispatch(getComments(id)),
