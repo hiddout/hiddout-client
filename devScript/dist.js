b1ad37fd9a3c7b2d5fa1e43c08fd25faddbcbdbc
TyrealGray <tyrealgray@ymail.com>
2019-05-02 23:36:56 +0100

    update build script

--- devScript/dist.js
+++ devScript/dist.js
@@ -1,10 +1,57 @@
 const path = require('path');
 const fsExtra = require('fs-extra');
+const archiver = require('archiver');
 
-const webFolderSrc = 'public';
-
-const distPath = path.join(process.cwd(), 'dist');
-const webSrc = path.join(process.cwd(), webFolderSrc);
+const rootPath = process.cwd();
+const distPath = path.join(rootPath, 'dist');
 
 fsExtra.emptyDirSync(distPath);
-fsExtra.copySync(webSrc, path.join(distPath, webFolderSrc));
+
+const output = path.join(distPath, 'public.zip');
+
+// create a file to stream archive data to.
+var outputFile = fsExtra.createWriteStream(output);
+var archive = archiver('zip', {
+	zlib: { level: 9 }, // Sets the compression level.
+});
+
+// listen for all archive data to be written
+// 'close' event is fired only when a file descriptor is involved
+outputFile.on('close', function() {
+	console.log(archive.pointer() + ' total bytes');
+	console.log(
+		'archiver has been finalized and the output file descriptor has closed.',
+	);
+});
+
+// This event is fired when the data source is drained no matter what was the data source.
+// It is not part of this library but rather from the NodeJS Stream API.
+// @see: https://nodejs.org/api/stream.html#stream_event_end
+outputFile.on('end', function() {
+	console.log('Data has been drained');
+});
+
+// good practice to catch warnings (ie stat failures and other non-blocking errors)
+archive.on('warning', function(err) {
+	if (err.code === 'ENOENT') {
+		// log warning
+	} else {
+		// throw error
+		throw err;
+	}
+});
+
+// good practice to catch this error explicitly
+archive.on('error', function(err) {
+	throw err;
+});
+
+// pipe archive data to the file
+archive.pipe(outputFile);
+
+// append files from a sub-directory, putting its contents at the root of archive
+archive.directory('public/');
+
+// finalize the archive (ie we are done appending files but streams have to finish yet)
+// 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+archive.finalize();
--- devScript/prebuild.js
+++ devScript/prebuild.js
@@ -1,11 +1,28 @@
 const path = require('path');
 const fsExtra = require('fs-extra');
 
+const rootPath = process.cwd();
+
 const webFolderSrc = 'public';
 
-const prebuildPath = path.join(process.cwd(), 'prebuild' , webFolderSrc);
-const webSrc = path.join(process.cwd(), webFolderSrc);
+const prebuildPath = path.join(rootPath, 'prebuild' , webFolderSrc);
+const webSrc = path.join(rootPath, webFolderSrc);
 
 fsExtra.emptyDirSync(webSrc);
 
 fsExtra.copySync( prebuildPath , webSrc);
+
+const extensionPath = path.join(webSrc, 'js','hiddout-extension.js');
+
+const source = fsExtra.readFileSync(path.join(rootPath, './node_modules/hiddout-viewer/dist/hiddoutViewer.js')).toString();
+
+const version = require('../package.json').dependencies['hiddout-viewer'];
+
+const template = `onmessage = function(e) {
+	const viewer = "${source}";
+	console.log('hiddout ${version}');
+	postMessage(viewer);
+};`;
+
+
+fsExtra.appendFileSync(extensionPath, template);
