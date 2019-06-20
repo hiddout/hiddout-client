5b52c1720fa75229623dffc0b18a5a3bb9ea4d08
Tyreal Gray <tyrealgray@ymail.com>
2019-06-20 21:30:56 +0100

    add subscription data

---
+++ src/actions/noticeboardAction.js
@@ -0,0 +1,31 @@
+import { GET_SUBSCRIPTION, REQUEST_GET_SUBSCRIPTION } from './actionType';
+import { config } from '../config';
+import { checkAuth } from './loginAction';
+
+export const getSubscriptio = () => {
+	return (dispatch, getState) => {
+		const { auth } = getState();
+		dispatch({ type: REQUEST_GET_SUBSCRIPTION });
+
+		return hiddoutViewer
+			.request(`${config.baseURL}${config.apiV1}user/subscription`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8',
+					authorization: auth.token,
+				},
+			})
+			.then((res) => {
+				console.log(res);
+				return dispatch(
+					checkAuth(res.status, {
+						type: GET_SUBSCRIPTION,
+						payload: { subscriptions: res.subscriptions },
+					}),
+				);
+			})
+			.catch((e) => {
+				console.error(e);
+			});
+	};
+};
--- src/reducers/account.js
+++ src/reducers/account.js
@@ -1,9 +1,9 @@
 // @flow
-import { LOGIN, LOGOUT, SIGN_UP } from '../actions/actionType';
+import { GET_SUBSCRIPTION, LOGIN, LOGOUT, SIGN_UP } from '../actions/actionType';
 
-export type AccountState = { user: string };
+export type AccountState = { user: string, subscriptions: Array<Object> };
 
-const account = (state: AccountState = {user: '' }, action: Action) =>
+const account = (state: AccountState = {user: '', subscriptions:[] }, action: Action) =>
 	immer.produce(state, draft => {
 
 		const payload = action.payload || {};
@@ -15,6 +15,9 @@ const account = (state: AccountState = {user: '' }, action: Action) =>
 				break;
 			case LOGOUT:
 				draft.user = '';
+				break;
+			case GET_SUBSCRIPTION:
+				draft.subscriptions = payload.subscriptions;
 				break;
 		}
 	});
