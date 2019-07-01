67634d50ccd047fca15cd98da7353069ff853290
TyrealGray <tyrealgray@ymail.com>
2019-07-01 22:54:43 +0100

    Adding show message feature

--- src/pages/messagePage/Message.js
+++ src/pages/messagePage/Message.js
@@ -4,25 +4,77 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
+import { Container, Segment, List } from 'semantic-ui-react';
+
 const NavigationBar = React.lazy(() =>
 	import('../../containers/navigationBar/NavigationBar'),
 );
 
 import type { Node } from 'react';
-import { Container, Segment } from 'semantic-ui-react';
+import type { AccountState } from '../../reducers/account';
+import { subscribePost } from '../../actions/postAction';
 
-type Props = {};
+type Props = {
+	history: Object,
+	account: AccountState,
+	subscribePost: (Object) => any,
+};
 
 type State = {};
 
 class Message extends React.Component<Props, State> {
 	render(): Node {
+
+		const {subscriptionMessages} = this.props.account;
+
 		return (
 			<React.Fragment>
 				<NavigationBar showBackBtn={'back'} />
 				<Container className={'PageContent'} textAlign={'left'}>
 					<Segment>
-						<h2>Messages</h2>
+						{subscriptionMessages && !subscriptionMessages.length && <h3> No new message</h3>}
+						{subscriptionMessages && subscriptionMessages.length > 0 && <List selection verticalAlign="middle">
+							{subscriptionMessages.map(
+								(message, i) => {
+									return (
+										<List.Item
+											key={i}
+											onClick={async () => {
+												const encodeId = hiddoutViewer.encodeId(
+													message.subscriptionId,
+												);
+												await this.props.subscribePost({
+													id: encodeId,
+													isSubscribed: true,
+													type: 'post',
+												});
+												this.props.history.push(
+													`/p/${encodeId}`,
+												);
+											}}
+										>
+											<List.Icon
+												name={
+													message.type === 'post'
+														? 'quote left'
+														: 'comment alternate'
+												}
+											/>
+											<List.Content>
+												<List.Header>
+													{message.content}
+												</List.Header>
+												<List.Description>
+													{' '}
+													This post you subscribed
+													just got updated{' '}
+												</List.Description>
+											</List.Content>
+										</List.Item>
+									);
+								},
+							)}
+						</List>}
 					</Segment>
 				</Container>
 			</React.Fragment>
@@ -32,11 +84,13 @@ class Message extends React.Component<Props, State> {
 
 const mapStateToProps = (state) => {
 	return {
+		account: state.account,
 	};
 };
 
 const mapDispatchToProps = (dispatch) => {
 	return {
+		subscribePost: (subscriptionData) => dispatch(subscribePost(subscriptionData)),
 	};
 };
 
