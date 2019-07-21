08ff0bce044000d8e9a820d63952ce07576a6947
Tyreal Gray <tyrealgray@ymail.com>
2019-07-21 02:23:17 +0100

    update viewer by a new solution

--- devScript/prebuild.js
+++ devScript/prebuild.js
@@ -11,18 +11,3 @@ const webSrc = path.join(rootPath, webFolderSrc);
 fsExtra.emptyDirSync(webSrc);
 
 fsExtra.copySync( prebuildPath , webSrc);
-
-const extensionPath = path.join(webSrc, 'js','hiddout-extension.js');
-
-const source = fsExtra.readFileSync(path.join(rootPath, './node_modules/hiddout-viewer/dist/hiddoutViewer.js')).toString();
-
-const version = require('../package.json').dependencies['hiddout-viewer'];
-
-const template = `onmessage = function(e) {
-	const viewer = "${source}";
-	console.log('hiddout ${version}');
-	postMessage(viewer);
-};`;
-
-
-fsExtra.appendFileSync(extensionPath, template);
--- src/main.js
+++ src/main.js
@@ -2,20 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const viewerWorker = new Worker('/public/js/hiddout-extension.js');
-
-viewerWorker.postMessage('init');
-
-viewerWorker.onmessage = (e) => {
-	if (typeof hiddoutViewer !== 'undefined') {
-		return;
-	}
-
-	const template = `<script type="text/javascript" id="viewerScriptTag">${e.data}var hiddoutViewer = window.index.HiddoutViewer;</script>`;
-	const frag = document.createRange().createContextualFragment(template);
-	const head = document.getElementsByTagName('head').item(0);
-	head.appendChild(frag);
-};
+const template = `<script type="text/javascript" id="viewerScriptTag">var hiddoutViewer = window.index.HiddoutViewer;</script>`;
+const frag = document.createRange().createContextualFragment(template);
+const head = document.getElementsByTagName('head').item(0);
+head.appendChild(frag);
 
 const interval = setInterval(() => {
 	if (typeof hiddoutViewer !== 'undefined') {
